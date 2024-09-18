925bcfc0f2d6449f4647096dccc04539d0fd3147
dawoodcule <95921352+dawoodcule@users.noreply.github.com>
2024-09-18 21:58:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,83 @@
+let userForm = document.getElementById("user-form")
+
+const retrieveEntries = () => {
+    let entries = localStorage.getItem("user-entries")
+    if(entries){
+        entries = JSON.parse(entries)
+    }
+    else{
+        entries=[]
+    }
+    return entries
+}
+let userEntries = retrieveEntries()
+
+const displayEntries = () => {
+    const entries = retrieveEntries();
+    const tableEntries = entries.map((entry) =>{
+        const nameCell = `<td class = 'border px-4 py-2'>${entry.name}</td>`
+        const emailCell = `<td class = 'border px-4 py-2'>${entry.email}</td>`
+        const passwordCell = `<td class = 'border px-4 py-2'>${entry.password}</td>`
+        const dobCell = `<td class = 'border px-4 py-2'>${entry.dob}</td>`
+        const acceptTermsCell = `<td class = 'border px-4 py-2'>${entry.acceptedTermsAndconditions}</td>`
+
+        const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`
+        return row
+
+    }).join("\n")
+
+const table = `<table class = "table-auto w-full"></tr>
+
+<th class= "px-4 py-2">Name</th>
+<th class= "px-4 py-2">Email</th>
+<th class= "px-4 py-2">Password</th>
+<th class= "px-4 py-2">Dob</th>
+<th class= "px-4 py-2">Accepted terms?</th>
+</tr>${tableEntries}</table> `
+let details = document.getElementById("user-entries")
+details.innerHTML = table
+}
+const saveUserForm = (event) => {
+    event.preventDefault()
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+
+    const acceptedTermsAndconditions = document.getElementById("acceptedTerms").checked;
+
+    const entry = {
+        name,
+        email,
+        password,
+        dob,
+        acceptedTermsAndconditions
+    }
+
+    userEntries.push(entry)
+
+    localStorage.setItem("user-entries",JSON.stringify(userEntries))
+    displayEntries()
+}
+userForm.addEventListener("submit",saveUserForm)
+displayEntries()
+
+function setDateLimits() {
+    const today = new Date();
+    const minDate = new Date();
+    const maxDate = new Date();
+
+
+    minDate.setFullYear(today.getFullYear() - 55);
+
+    maxDate.setFullYear(today.getFullYear() - 18);
+
+
+    const minDateString = minDate.toISOString().split('T')[0];
+    const maxDateString = maxDate.toISOString().split('T')[0];
+
+    document.getElementById('dob').setAttribute('min', minDateString);
+    document.getElementById('dob').setAttribute('max', maxDateString);
+}
+
+window.onload = setDateLimits; 
